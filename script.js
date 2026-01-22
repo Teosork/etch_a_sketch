5c1b05475bd6cf15cbb38e2e20163450d9d7ad93
Teosork <76114875+Teosork@users.noreply.github.com>
2026-01-22 18:41:14 +0100

    Creating the default Grid (16x16) through js and do the basic styling

---
+++ script.js
@@ -0,0 +1,18 @@
+const container = document.querySelector("#container");
+
+function createGrid(size) {
+  container.innerHTML = "";
+
+  const total = size * size;
+  const squareSize = 960 / size;
+
+  for (let i = 0; i < total; i++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    square.style.width = `${squareSize}px`;
+    square.style.height = `${squareSize}px`;
+    container.appendChild(square);
+  }
+}
+
+createGrid(16);
