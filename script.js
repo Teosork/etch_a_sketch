04ba84166b36440d0b02f7b1a1d5cff5ecbae91a
Teosork <76114875+Teosork@users.noreply.github.com>
2026-01-22 20:15:02 +0100

    Add resize button with prompt + integer validation (1–100)

--- script.js
+++ script.js
@@ -23,3 +23,22 @@ container.addEventListener("mouseover", (event) => {
         hovered.style.backgroundColor = "black";
     }
 });
+
+resizeBtn.addEventListener("click", () => {
+   const gridNumber = prompt("Please enter the grid size (1–100):");
+    if (gridNumber === null) {
+        return;
+    }
+    const size = Number(gridNumber);
+
+    if (!Number.isInteger(size)) {
+    alert("Please enter a whole number (integer).");
+    return;
+    }
+    if (size < 1 || size > 100) {
+    alert("Invalid number. Minimum is 1 and maximum is 100.");
+    return;
+  }
+
+  createGrid(size);
+});
